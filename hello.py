b3c5c695ca7517883375835306cd869cc0ae8b30
Raymond Nivar <nivarraymond@gmail.com>
2024-09-13 16:58:05 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+# create a Flask Instance
+app = Flask(__name__)
+
+# create a route decorator
+@app.route('/')
+def index():
+    first_name = "Ray"
+    stuff = "This is bold text"
+    fav_pizza = ["Pepperoni", "Cheese", "Mushrooms",  41 ]
+    return render_template("index.html", first_name=first_name, stuff=stuff, fav_pizza=fav_pizza)
+
+# localhost:5000/user/ray
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+# Customer Error Pages
+# 
+#  Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#  Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
